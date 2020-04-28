93a540e8faf90a998c415a3c414aacbb61740258
Dominik Lewczyński <dominiklew200060@gmail.com>
2020-04-28 19:01:47 +0200

    Update Zad_12.py

--- wd_cw05/Zad_12.py
+++ wd_cw05/Zad_12.py
@@ -1,8 +1,8 @@
-def miesiace():
+def lista_miesiacy():
     lista_miesiecy = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień','Wrzesień', 'Październik', 'Listopad', 'Grudzień']
     for i in range(0, 12, 1):
         yield lista_miesiecy[i]
 
-miesiac = miesiace()
+miesiace = lista_miesiacy()
 for i in range(0, 12, 1):
-    print(next(miesiac))
+    print(next(miesiace))
