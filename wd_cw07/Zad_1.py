46e92c1a891f2b23274bf3fe7c5d6931e4827f8a
Dominik Lewczyński <dominiklew200060@gmail.com>
2020-05-12 18:58:10 +0200

    Update Zad_1.py

--- wd_cw07/Zad_1.py
+++ wd_cw07/Zad_1.py
@@ -3,6 +3,6 @@ import numpy as np
 macierz_1 = np.array([1, 2, 3])
 macierz_2 = np.array([4, 5, 6])
 print('macierz_1 * macierz_2:')
-print(macierz_1*macierz_2)
+print(macierz_1," * ",macierz_2," = ",macierz_1*macierz_2)
 print('macierz_2 * macierz_1:')
-print(macierz_2*macierz_1)
+print(macierz_2," * ",macierz_1," = ",macierz_2*macierz_1)
