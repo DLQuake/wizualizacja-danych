9de3a40a4b2d15416fb6109a4a08675ed45eae0d
Dominik Lewczyński <dominiklew200060@gmail.com>
2020-05-21 12:50:23 +0200

    Update Zad_1.py

--- wd_cw09/Zad_1.py
+++ wd_cw09/Zad_1.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import xlrd
+import openpyxl
 
 ts = pd.read_excel('datasets/imiona.xlsx')
-
+print(ts)
 ts = ts.cumsum()
 print(ts)
 ts.plot()
