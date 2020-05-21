84e5e53dedab67c95ad5f9e3e1ceb34dcaa2cf3f
Dominik Lewczyński <dominiklew200060@gmail.com>
2020-05-21 12:48:09 +0200

    Update Zad_1.py and add Do_Zad_1.xlsx

--- wd_cw08/Zad_1.py
+++ wd_cw08/Zad_1.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import xlrd
+import openpyxl
 
 df = pd.read_excel('datasets/imiona.xlsx')
-dg=pd.DataFrame(df)
-print(dg)
+print(df)
+df.to_excel('Do_Zad_1.xlsx',  sheet_name='Zadanie 1')
