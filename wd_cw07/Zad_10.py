e5e077cedd29c926cb1a6fa1c4faf898472c4338
Dominik Lewczyński <dominiklew200060@gmail.com>
2020-05-30 13:02:37 +0200

    Add Zad_8.py, Zad_9.py and Zad_10.py

---
+++ wd_cw07/Zad_10.py
@@ -0,0 +1,21 @@
+import numpy as np
+# generujemy macierz 1x6
+b = np.arange(81).reshape(9, 9)
+print(b)
+print(b.shape)
+print('przeksztalacamy ja na macierz 3x27')
+c = b.reshape((3,27))
+print(c)
+print(c.shape)
+print('przeksztalacamy ja na macierz 27x3')
+d = c.reshape((27,3))
+print(d)
+print(d.shape)
+print('spłaszczamy macierz zyskujac pierwotny kształt ze zmiennej b')
+e = d.ravel()
+print(e)
+print(e.shape)
+print('transpozycja macierzy')
+f = c.T
+print(f)
+print(f.shape)
---
+++ wd_cw07/Zad_8.py
@@ -0,0 +1,6 @@
+import numpy as np
+# generujemy macierz 3x3
+b = np.arange(9).reshape(3,3)
+print(b)
+for a in b:
+   print(a)
