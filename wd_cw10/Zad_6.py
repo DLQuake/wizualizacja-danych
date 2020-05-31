582cd980e8cc2d78b5d753964f6477d62394ccea
Dominik Lewczyński <dominiklew200060@gmail.com>
2020-05-31 20:19:34 +0200

    Update Zad_6.py

--- wd_cw10/Zad_6.py
+++ wd_cw10/Zad_6.py
@@ -1,26 +1,24 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import xlrd
+import openpyxl
+
+xlsx = pd.ExcelFile('datasets/imiona.xlsx')
+df = pd.read_excel(xlsx, 'Arkusz1')
+dane_wyk_1 = df.groupby(['Plec']).agg({'Liczba': 'sum'}).reset_index()
+dane_wyk_2 = df.groupby(['Plec', 'Rok']).agg({'Liczba': 'sum'}).reset_index()
+dane_wyk_3 = df.groupby(['Rok']).agg({'Liczba': 'sum'}).reset_index()
+
+plt.subplot(3,1,1)
+plt.bar(dane_wyk_1['Plec'], dane_wyk_1['Liczba'])
 
 
-etykiety = ['K', 'M']
-wartosci = [100, 50]
-
-plt.bar(etykiety, wartosci)
-plt.subplot(3, 2, 1)
-plt.xticks(rotation=45)
-plt.ylabel('Ilość')
-plt.xlabel('Płeć')
-plt.show()
-
-plt.plot(etykiety, wartosci)
-plt.subplot(3, 2, 4)
-plt.ylabel('Liczba')
-plt.xlabel('Plec')
+plt.subplot(3,1,2)
+plt.plot(dane_wyk_2['Rok'].unique(), dane_wyk_2[dane_wyk_2['Plec'] == 'K']['Liczba'], label='Kobiety')
+plt.plot(dane_wyk_2['Rok'].unique(), dane_wyk_2[dane_wyk_2['Plec'] == 'M']['Liczba'], label='Mężczyżni')
 plt.legend()
-plt.show()
 
-plt.bar(etykiety, wartosci)
-plt.subplot(325)
-plt.ylabel('Liczba')
-plt.xlabel('Plec')
+plt.subplot(3,1,3)
+plt.bar(dane_wyk_3['Rok'], dane_wyk_3['Liczba'])
 plt.show()
